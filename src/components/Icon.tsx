c574f7a864dd066e02a2f46bd99a1ee2c4cbb978
xxxjinn <tlsguswls319@naver.com>
2025-05-26 22:32:38 +0900

    refactor: Icon 컴포넌트 memoization

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,6 +1,7 @@
 import { ICON_SIZE_MAP } from "@/constants/components/constants";
 import { IconProps } from "@/types/components/types";
 import Image from "next/image";
+import React from "react";
 
 const Icon = ({ src, alt, size, className }: IconProps) => {
   const pixelSize = ICON_SIZE_MAP[size];
@@ -16,4 +17,4 @@ const Icon = ({ src, alt, size, className }: IconProps) => {
   );
 };
 
-export default Icon;
+export default React.memo(Icon);
