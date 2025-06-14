0fe4110a0b8034b99b8b5f028103e4284ad49c29
xxxjinn <tlsguswls319@naver.com>
2025-06-14 20:52:49 +0900

    feat: 헤더 text 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="flex h-14 min-h-14 items-center justify-between bg-white px-4">
       <Icon src={GovernmentIcon} alt="gorvernment-icon" size="lg" />
       <h1 className="absolute left-1/2 -translate-x-1/2 text-center text-lg font-semibold">
-        국민비서 민서
+        통관 챗봇 관식이
       </h1>
       <Button className="bg-navy-950" onClick={() => window.location.reload()}>
         {REFRESH_BUTTON}
