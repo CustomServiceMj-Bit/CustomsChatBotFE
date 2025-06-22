27e60cd47283964f118821549b7844250aceae78
xxxjinn <tlsguswls319@naver.com>
2025-06-22 15:35:36 +0900

    refactor: 불필요한 import 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@
 import ChatContainer from "@/components/chat/ChatContainer";
 import Header from "@/components/Header";
 import InputContainer from "@/components/InputContainer";
-import { handleTrackingCustomerClearance } from "@/lib/chat/handlers/handleTrackingCustomerClearance";
-import { useChatMessageStore } from "@/store/useStore";
-import { useState } from "react";
 
 export default function Home() {
   return (
