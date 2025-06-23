da3ac7b0605b2157dd0aa2f130df8ceff7aceb73
xxxjinn <tlsguswls319@naver.com>
2025-06-23 22:59:37 +0900

    feat: api 수정에 따른 api 연결 관련 코드 수정

--- src/api/getTrackingStatus.ts
+++ src/api/getTrackingStatus.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import axiosInstance from "./instance";
+import { BotResponse } from "@/types/api/types";
 
 export const getTrackingCustomerClearance = async (cargoNumber: string) => {
   try {
@@ -12,6 +14,21 @@ export const getTrackingCustomerClearance = async (cargoNumber: string) => {
     return response.data;
   } catch (error) {
     console.error("배송 조회 요청 실패:", error);
+    throw error;
+  }
+};
+
+export const postQuestionToBot = async (
+  text: string,
+): Promise<AxiosResponse<BotResponse>> => {
+  try {
+    const response = await axiosInstance.post(`/api/chat`, {
+      message: text,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("api 요청 실패:", error);
     throw error;
   }
 };
---
+++ src/types/api/types.ts
@@ -0,0 +1,5 @@
+export interface BotResponse {
+  data: {
+    reply: string;
+  };
+}
