ea2c5888268735398b439c030f853a1eea90e47a
xxxjinn <tlsguswls319@naver.com>
2025-06-25 22:20:51 +0900

    feat: react-markdown 적용

--- src/components/chat/MarkdownAnswer.tsx
+++ src/components/chat/MarkdownAnswer.tsx
@@ -1,5 +1,12 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 const MarkdownAnswer = ({ answer }: { answer: string }) => {
-  return <div>{answer}</div>;
+  return (
+    <div className="w-fit break-keep whitespace-pre-line">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{answer}</ReactMarkdown>
+    </div>
+  );
 };
 
 export default MarkdownAnswer;
