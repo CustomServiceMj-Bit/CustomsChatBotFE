53a86a6bd2a24d9adf696cfb1dd15c1eaff92dc0
xxxjinn <tlsguswls319@naver.com>
2025-06-26 23:03:30 +0900

    style: typingBlock 폰트 크기 수정

--- src/components/chat/TypingBlock.tsx
+++ src/components/chat/TypingBlock.tsx
@@ -2,7 +2,7 @@ import { BOT_TYPING } from "@/constants/texts";
 
 const TypingBlock = () => {
   return (
-    <div className="w-[90px]">
+    <div className="w-[100px] text-sm">
       {BOT_TYPING}
       <span className="typing-dots" />
     </div>
