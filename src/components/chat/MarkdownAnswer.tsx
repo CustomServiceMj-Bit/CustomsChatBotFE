f7e706f5feeff3385105e9e495745119dde774cc
xxxjinn <tlsguswls319@naver.com>
2025-06-30 17:19:35 +0900

    feat: 마크다운에 rehype-sanitize 적용

--- src/components/chat/MarkdownAnswer.tsx
+++ src/components/chat/MarkdownAnswer.tsx
@@ -1,10 +1,16 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
 
 const MarkdownAnswer = ({ answer }: { answer: string }) => {
   return (
     <div className="text-sm break-keep whitespace-pre-line">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{answer}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeSanitize]}
+      >
+        {answer}
+      </ReactMarkdown>
     </div>
   );
 };
