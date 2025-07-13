0a25596d147abc34b9883c154672b59846401b90
xxxjinn <tlsguswls319@naver.com>
2025-07-13 14:57:05 +0900

    style: 불필요한 글자 크기 스타일 삭제

--- src/components/QuestionChipList.tsx
+++ src/components/QuestionChipList.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const QuestionChipList = ({ list, onChipClick }: QuestionChipListProps) => {
   return (
-    <div className="mt-1 ml-12 flex w-[290px] flex-wrap gap-2 self-start rounded-lg py-2 text-sm">
+    <div className="mt-1 ml-12 flex w-[290px] flex-wrap gap-2 self-start rounded-lg py-2">
       {list.map((value, index) => {
         return (
           <div key={index}>
