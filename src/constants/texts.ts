55b4254a16d6d4a824b39ccacf6902883363e187
HyunJin <102955516+xxxjinn@users.noreply.github.com>
2025-07-20 17:12:00 +0900

    refactor: texts.ts 업데이트

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -1,7 +1,7 @@
 export const INIT_BOT_MESSAGE =
   "안녕하세요! **올인원 관세 에이전트 관식이**입니다. `버튼`을 선택하거나, `대화창`에 질문해주세요.";
 
-export const HEADER_TEXT = "올인원 에이전트 챗봇 관식이";
+export const HEADER_TEXT = "올인원 에이전트 챗봇";
 
 export const QUICK_QUESTION_CHIPS = [
   "관세를 예측해줘",
